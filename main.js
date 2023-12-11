94ecab9a59097202a66037879a449fa93314634e
Likhaya <99352117+likeLikhaya@users.noreply.github.com>
2023-12-11 15:37:28 +0200

    Comments for js code

---
+++ main.js
@@ -0,0 +1,19 @@
+// Navigation Bar
+
+// Services Section
+
+// Projects Section
+
+// Clients review
+
+//Dark/Light theme
+
+// Navigation menu items
+
+//Navigation Menu
+
+//Scroll animations reveal
+
+//Common reveal options for animations
+
+//Target element for specific animations
