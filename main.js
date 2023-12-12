381884a49caca30b5349ba304c7beb04ac748e66
Likhaya <99352117+likeLikhaya@users.noreply.github.com>
2023-12-12 15:36:04 +0200

    Education & Skills section

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 // Navigation Bar
 
+window.addEventListener("scroll", function(){
+    const header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 0);
+});
+
 // Services Section
 
 // Projects Section
