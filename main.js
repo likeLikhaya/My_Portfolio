d09837c40cb38c81734b9685cb8398d8b44a2e00
Likhaya <kalimashelikhaya3@gmail.com>
2024-01-16 21:50:20 +0200

    dark theme & scroll to top btn

--- main.js
+++ main.js
@@ -18,10 +18,35 @@ window.addEventListener("scroll", function(){
 const scrollTopBtn = document.querySelector(".scrollToTop-btn");
 
 window.addEventListener("scroll", function(){
-    scrollTopBtn.classList.toggle("")
+    scrollTopBtn.classList.toggle("active", this.window.scrollY > 500);
+
+});
+
+scrollTopBtn.addEventListener("click", () => {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+
 })
 
 // Navigation menu items
+
+window.addEventListener("scroll", () => {
+    const sections = document.querySelectorAll("section");
+    const scrollY = window.pageYOffset;
+
+    sections.forEach(current =>{
+        let sectionHeight = current.offsetHeight;
+        let sectTop = current.offsetTop - 50;
+        let id = current.getAttribute("id");
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector(".nav-items a[href*=" + id + "]").classList.add("active");
+        }
+        else{
+            document.querySelector(".nav-items a[href*=" + id + "]").classList.remove("active");
+        }
+    });
+});
 
 //Navigation Menu
 
